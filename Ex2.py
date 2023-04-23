e5c24ab7e9c34fe5213a3685c3b30895e4c62483
asryanani1997 <asryan.ani1997@gmail.com>
2023-04-23 16:23:36 +0400

    Fixed Exercise 2

--- Ex2.py
+++ Ex2.py
@@ -1,7 +1,7 @@
 def intWithinBounds (x,y,z):
-    if y<=x<z:
+    if type(x)==int and type(y)==int and type(z)==int and y<=x<z:
         return True
     else:
         return False
 
-print(intWithinBounds(6, 1, 6))
+print(intWithinBounds(3, 1.5, 9))
